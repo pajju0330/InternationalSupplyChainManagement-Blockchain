dfad0e969fa4daaecbbbde9fa4fa7f1ba4b02ebf
pajju <prajwalshah.2003@gmail.com>
2024-04-18 22:22:46 +0530

    some commit

--- server/scripts/deploy.js
+++ server/scripts/deploy.js
@@ -4,6 +4,8 @@ async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
   const RawMaterialJourney = await ethers.getContractFactory("RawMaterialJourney");
   const rawMaterialJourney = await RawMaterialJourney.deploy();
   await rawMaterialJourney.deployed();
